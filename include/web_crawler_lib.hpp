28ca3304c02bf8cd561aee3b206d3dd0f9540b6f
volpertinger <merzlovnik@mail.ru>
2021-01-11 23:40:55 +0300

    final_05

---
+++ include/web_crawler_lib.hpp
@@ -0,0 +1,8 @@
+// Copyright 2020 Merzlov Nikolay merzlovnik@mail.ru
+
+#pragma once
+
+#include <web_crawler_lib/definitions.hpp>
+#include <web_crawler_lib/parser.hpp>
+#include <web_crawler_lib/url_reader.hpp>
+#include <web_crawler_lib/web_crawler.hpp>
