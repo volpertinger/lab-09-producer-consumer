a0c66166da38967ba2ee145d8d93865b964cf774
volpertinger <merzlovnik@mail.ru>
2021-01-11 23:47:03 +0300

    final_06

--- tests/web_crawler_lib/url_reader_test.cpp
+++ tests/web_crawler_lib/url_reader_test.cpp
@@ -9,16 +9,16 @@ namespace web_crawler_lib {
     TEST(url_reader, parse_url_parts) {
         EXPECT_EQ(::std::make_optional(::std::make_pair(::std::string("github.com"), ::std::string("/"))),
                   parse_url_parts("http://github.com/"));
-        EXPECT_EQ(::std::make_optional(::std::make_pair(::std::string("github.com"), ::std::string("/JarvisCraft"))),
-                  parse_url_parts("http://github.com/JarvisCraft"));
+        EXPECT_EQ(::std::make_optional(::std::make_pair(::std::string("github.com"), ::std::string("/volpertinger"))),
+                  parse_url_parts("http://github.com/volpertinger"));
         EXPECT_EQ(::std::make_optional(::std::make_pair(::std::string("github.com"), ::std::string("/"))),
                   parse_url_parts("http://github.com"));
         EXPECT_EQ(::std::make_optional(::std::make_pair(::std::string("example.com"), ::std::string("/foo/bar/baz"))),
                   parse_url_parts("http://example.com/foo/bar/baz"));
         EXPECT_EQ(::std::make_optional(::std::make_pair(::std::string("github.com"), ::std::string("/"))),
                   parse_url_parts("github.com/"));
-        EXPECT_EQ(::std::make_optional(::std::make_pair(::std::string("github.com"), ::std::string("/JarvisCraft"))),
-                  parse_url_parts("github.com/JarvisCraft"));
+        EXPECT_EQ(::std::make_optional(::std::make_pair(::std::string("github.com"), ::std::string("/volpertinger"))),
+                  parse_url_parts("github.com/volpertinger"));
         EXPECT_EQ(::std::make_optional(::std::make_pair(::std::string("github.com"), ::std::string("/"))),
                   parse_url_parts("github.com"));
         EXPECT_EQ(::std::make_optional(::std::make_pair(::std::string("example.com"), ::std::string("/foo/bar/baz"))),
